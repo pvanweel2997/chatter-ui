e9904d4913f11088f327a91acf94b914638225a0
Patrick Van Weelden <pvanweel@yahoo.com>
2024-02-03 16:21:20 -0600

    saving changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
-import Auth from "./components/auth/Auth";
 import { RouterProvider } from "react-router-dom";
 import router from "./components/Routes";
 
