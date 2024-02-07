98921648a813dfd758fb661f4c8a99c47202bdf5
Patrick Van Weelden <pvanweel@yahoo.com>
2024-02-07 15:49:48 -0600

    hooked up login page to useLogin hook to call server

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,16 +1,18 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import { Link as MUILink } from "@mui/material";
-import Auth from "./Auth"
+import Auth from "./Auth";
+import { useLogin } from "../../hooks/useLogin";
 
 const Login = () => {
-    return (
-        <>
-            <Auth submitLabel="Login" onSubmit={async () => {}}>
-                <Link to={"/signup"} style={{ alignSelf: "center" }}>
-                    <MUILink>Signup</MUILink>
-                </Link>
-            </Auth>
-        </>
-    )
-}
-export default Login
+  const { login, error } = useLogin();
+  return (
+    <>
+      <Auth submitLabel="Login" onSubmit={(request) => login(request)}>
+        <Link to={"/signup"} style={{ alignSelf: "center" }}>
+          <MUILink>Signup</MUILink>
+        </Link>
+      </Auth>
+    </>
+  );
+};
+export default Login;
