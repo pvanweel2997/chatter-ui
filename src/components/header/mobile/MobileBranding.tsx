509c7682284ef434eed49d05ebf12a480f0829be
Patrick Van Weelden <pvanweel@yahoo.com>
2024-02-20 11:04:58 -0600

    created app menu bar

---
+++ src/components/header/mobile/MobileBranding.tsx
@@ -0,0 +1,34 @@
+import { Typography } from "@mui/material";
+import ForumIcon from "@mui/icons-material/Forum";
+import router from "../../Routes";
+
+const MobileBranding = () => {
+  return (
+    <>
+      <ForumIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+      <Typography
+        variant="h5"
+        noWrap
+        component="a"
+        onClick={() => {
+          router.navigate("/");
+        }}
+        sx={{
+          mr: 2,
+          display: { xs: "flex", md: "none" },
+          flexGrow: 1,
+          fontFamily: "monospace",
+          fontWeight: 700,
+          cursor: "pointer",
+          letterSpacing: ".3rem",
+          color: "inherit",
+          textDecoration: "none",
+        }}
+      >
+        CHATTER
+      </Typography>
+    </>
+  );
+};
+
+export default MobileBranding;
