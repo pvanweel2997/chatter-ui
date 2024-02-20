0b56cfe764474d181801d1071dd9b6d483988272
Patrick Van Weelden <pvanweel@yahoo.com>
2024-02-20 15:24:25 -0600

    created snackbar and reusable error messages

--- src/components/auth/Guard.tsx
+++ src/components/auth/Guard.tsx
@@ -2,18 +2,27 @@ import { useGetMe } from "../../hooks/useGetMe";
 import excludedRoutes from "../../constants/excluded-routes";
 import { useEffect } from "react";
 import { authenticatedVar } from "../../constants/authenticated";
+import { snackVar } from "../../constants/snack";
+import { UNKNOWN_ERROR_SNACK_MESSAGE } from "../../constants/error";
 interface GuardProps {
   children: JSX.Element;
 }
 
 const Guard = ({ children }: GuardProps) => {
-  const { data: user } = useGetMe();
+  const { data: user, error } = useGetMe();
 
   useEffect(() => {
     if (user) {
       authenticatedVar(true);
     }
   }, [user]);
+
+  useEffect(() => {
+    if (error?.networkError) {
+      snackVar(UNKNOWN_ERROR_SNACK_MESSAGE);
+    }
+  }, [error]);
+
   return (
     <>
       {excludedRoutes.includes(window.location.pathname)
