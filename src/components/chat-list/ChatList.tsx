6945c13792712e4252ef0a18f999eaf7583d7fa2
Patrick Van Weelden <pvanweel@yahoo.com>
2024-02-21 07:56:00 -0600

    created add chat modul

--- src/components/chat-list/ChatList.tsx
+++ src/components/chat-list/ChatList.tsx
@@ -1,48 +1,61 @@
-import * as React from "react";
+import react, { useState } from "react";
 import ChatListItem from "./chat-list-item/ChatListItem";
 import { Divider, List, Stack } from "@mui/material";
 import ChatListHeader from "./chat-list-header/ChatListHeader";
+import ChatListAdd from "./chat-list-add/ChatListAdd";
 
 const ChatList = () => {
+  const [chatListAddVisible, setChatListAddVisible] = useState(false);
+
   return (
-    <Stack>
-      <ChatListHeader />
-      <Divider />
-      <List
-        sx={{
-          width: "100%",
-          maxWidth: 360,
-          bgcolor: "background.paper",
-          maxHeight: "80vh",
-          overflow: "auto",
-        }}
-      >
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-        <ChatListItem />
-      </List>
-    </Stack>
+    <>
+      <ChatListAdd
+        open={chatListAddVisible}
+        handleClose={() => setChatListAddVisible(false)}
+      />
+      <Stack>
+        <ChatListHeader
+          handleAddChat={() => {
+            setChatListAddVisible(true);
+          }}
+        />
+        <Divider />
+        <List
+          sx={{
+            width: "100%",
+            maxWidth: 360,
+            bgcolor: "background.paper",
+            maxHeight: "80vh",
+            overflow: "auto",
+          }}
+        >
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+          <ChatListItem />
+        </List>
+      </Stack>
+    </>
   );
 };
 
