edda56c835078440219ed2cbfc895e7226031f5d
Patrick Van Weelden <pvanweel@yahoo.com>
2024-02-22 15:05:43 -0600

    changes to update messages cache

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -11,9 +11,9 @@ import {
   Stack,
   Typography,
 } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
 import { useCreateMessage } from "../../hooks/useCreateMessage";
 import { useEffect, useRef, useState } from "react";
-import SendIcon from "@mui/icons-material/Send";
 import { useGetMessages } from "../../hooks/useGetMessages";
 import { useMessageCreated } from "../../hooks/useMessageCreated";
 
@@ -21,21 +21,20 @@ const Chat = () => {
   const params = useParams();
   const [message, setMessage] = useState("");
   const chatId = params._id!;
-  const [createMessage] = useCreateMessage(chatId);
   const { data } = useGetChat({ _id: chatId });
+  const [createMessage] = useCreateMessage();
   const { data: messages } = useGetMessages({ chatId });
-  const { data: latestMessage } = useMessageCreated({ chatId });
   const divRef = useRef<HTMLDivElement | null>(null);
   const location = useLocation();
 
-  const scrollToBottom = () => divRef.current?.scrollIntoView();
+  useMessageCreated({ chatId });
 
-  console.log("=== latestMessage", latestMessage);
+  const scrollToBottom = () => divRef.current?.scrollIntoView();
 
   useEffect(() => {
     setMessage("");
     scrollToBottom();
-  }, [location, messages]);
+  }, [location.pathname, messages]);
 
   const handleCreateMessage = async () => {
     await createMessage({
@@ -49,25 +48,35 @@ const Chat = () => {
     <Stack sx={{ height: "100%", justifyContent: "space-between" }}>
       <h1>{data?.chat.name}</h1>
       <Box sx={{ maxHeight: "70vh", overflow: "auto" }}>
-        {messages?.messages.map((message) => (
-          <Grid container alignItems="center" marginBottom="1rem">
-            <Grid item xs={2} lg={1}>
-              <Avatar src="" sx={{ width: 52, height: 52 }} />
-            </Grid>
-            <Grid item xs={10} lg={11}>
-              <Stack>
-                <Paper sx={{ width: "fit-content" }}>
-                  <Typography sx={{ padding: "0.9rem" }}>
-                    {message.content}
-                  </Typography>
-                </Paper>
-                <Typography variant="caption" sx={{ ml: "0.25rem" }}>
-                  {new Date(message.createdAt).toLocaleTimeString()}
-                </Typography>
-              </Stack>
-            </Grid>
-          </Grid>
-        ))}
+        {messages &&
+          [...messages.messages]
+            .sort(
+              (messageA, messageB) =>
+                new Date(messageA.createdAt).getTime() -
+                new Date(messageB.createdAt).getTime()
+            )
+            .map((message) => (
+              <Grid container alignItems="center" marginBottom="1rem">
+                <Grid item xs={2} lg={1}>
+                  <Avatar src="" sx={{ width: 52, height: 52 }} />
+                </Grid>
+                <Grid item xs={10} lg={11}>
+                  <Stack>
+                    <Paper sx={{ width: "fit-content" }}>
+                      <Typography sx={{ padding: "0.9rem" }}>
+                        {message.content}
+                      </Typography>
+                    </Paper>
+                    <Typography
+                      variant="caption"
+                      sx={{ marginLeft: "0.25rem" }}
+                    >
+                      {new Date(message.createdAt).toLocaleTimeString()}
+                    </Typography>
+                  </Stack>
+                </Grid>
+              </Grid>
+            ))}
         <div ref={divRef}></div>
       </Box>
       <Paper
@@ -82,16 +91,16 @@ const Chat = () => {
       >
         <InputBase
           sx={{ ml: 1, flex: 1, width: "100%" }}
-          placeholder="Message"
           onChange={(event) => setMessage(event.target.value)}
+          value={message}
+          placeholder="Message"
           onKeyDown={async (event) => {
             if (event.key === "Enter") {
               await handleCreateMessage();
             }
           }}
-          value={message}
         />
-        <Divider />
+        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
         <IconButton
           onClick={handleCreateMessage}
           color="primary"
