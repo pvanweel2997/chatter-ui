03aab079f40e64dd85229f971dfb5c7313851b21
Patrick Van Weelden <pvanweel@yahoo.com>
2024-02-27 08:47:01 -0600

    added ability to upload image to amazon s3 bucket

--- src/components/chat-list/chat-list-item/ChatListItem.tsx
+++ src/components/chat-list/chat-list-item/ChatListItem.tsx
@@ -25,7 +25,7 @@ const ChatListItem = ({ chat, selected }: ChatListProps) => {
           selected={selected}
         >
           <ListItemAvatar>
-            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            <Avatar alt="Remy Sharp" src={chat.latestMessage?.user.imageUrl} />
           </ListItemAvatar>
           <ListItemText
             primary={chat.name}
